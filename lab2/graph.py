04c31c29649f43f9b4a3937cdb2acfcf96e859a0
Матвей Молчанов <trhorse2004@gmail.com>
2026-09-24 14:12:30 +0300

    done with 1 and 2 i guess

--- lab2/graph.py
+++ lab2/graph.py
@@ -4,12 +4,13 @@ from sample import generateNormalPopulation, generatePopulation, generateSample
 from scipy.stats import gaussian_kde, norm, uniform
 
 
-def makeGraph(population, sampleSize, trueDensity, xRange, title="graph", gridSize=400):
+def makeGraph(population, sampleSize, trueDensity, xRange, title="graph", h=None):
     sample = np.array(generateSample(population, sampleSize))
-    kde = gaussian_kde(sample, bw_method="silverman")
-    h = kde.factor * np.std(sample, ddof=1)
+    s = np.std(sample, ddof=1)
+    kde = gaussian_kde(sample, bw_method="silverman" if h is None else h / s)
+    h = kde.factor * s
 
-    x = np.linspace(*xRange, gridSize)
+    x = np.linspace(*xRange, 400)
 
     plt.plot(x, kde(x), label=f"KDE (h = {h:.4f})")
     plt.plot(x, trueDensity(x), color="red", linestyle="--", label="f(x)")
@@ -29,12 +30,12 @@ normalPopulation = generateNormalPopulation(mathExpect, dispersion, 10**6)
 uniformDensity = uniform(0, 1).pdf
 normalDensity = norm(mathExpect, np.sqrt(dispersion)).pdf
 
-makeGraph(population, 10**1, uniformDensity, (-0.2, 1.2), f"Uniform, n = {10**1}")
-makeGraph(population, 10**2, uniformDensity, (-0.2, 1.2), f"Uniform, n = {10**2}")
-makeGraph(population, 10**3, uniformDensity, (-0.2, 1.2), f"Uniform, n = {10**3}")
-makeGraph(population, 10**4, uniformDensity, (-0.2, 1.2), f"Uniform, n = {10**4}")
+makeGraph(population, 10**1, uniformDensity, (-1, 2), f"Uniform, n = {10**1}")
+makeGraph(population, 10**2, uniformDensity, (-1, 2), f"Uniform, n = {10**2}")
+makeGraph(population, 10**3, uniformDensity, (-1, 2), f"Uniform, n = {10**3}")
+makeGraph(population, 10**4, uniformDensity, (-1, 2), f"Uniform, n = {10**4}")
 
-makeGraph(normalPopulation, 10**1, normalDensity, (-4, 4), f"Normal, n = {10**1}")
-makeGraph(normalPopulation, 10**2, normalDensity, (-4, 4), f"Normal, n = {10**2}")
-makeGraph(normalPopulation, 10**3, normalDensity, (-4, 4), f"Normal, n = {10**3}")
-makeGraph(normalPopulation, 10**4, normalDensity, (-4, 4), f"Normal, n = {10**4}")
+makeGraph(normalPopulation, 10**1, normalDensity, (-5, 5), f"Normal, n = {10**1}")
+makeGraph(normalPopulation, 10**2, normalDensity, (-5, 5), f"Normal, n = {10**2}")
+makeGraph(normalPopulation, 10**3, normalDensity, (-5, 5), f"Normal, n = {10**3}")
+makeGraph(normalPopulation, 10**4, normalDensity, (-5, 5), f"Normal, n = {10**4}")
--- lab2/histogram.py
+++ lab2/histogram.py
@@ -1,35 +1,47 @@
 import math
 
 import matplotlib.pyplot as plt
+import numpy as np
 from sample import generateNormalPopulation, generatePopulation, generateSample
+from scipy.stats import norm, uniform
 
 
-def makeHistogram(population, sampleSize, title="Histogram"):
+def makeHistogram(population, sampleSize, trueDensity, xRange, title="Histogram"):
     sample = sorted(generateSample(population, sampleSize))
     plt.hist(
         sample,
         bins=round(1 + 1.59 * math.log(sampleSize)),
         color="skyblue",
         edgecolor="black",
+        density=True,
+        label="Histogram",
     )
-    # plt.axhline(y=1, color="red", linestyle="--", label="Uniform density")
-    # plt.legend()
+
+    x = np.linspace(*xRange, 400)
+    plt.plot(x, trueDensity(x), color="red", linestyle="--", label="f(x)")
+
+    plt.legend()
     plt.title(title)
     plt.xlabel("Value")
-    plt.ylabel("Frequency")
+    plt.ylabel("Density")
     plt.show()
 
 
-# population = generatePopulation(0, 1, 10**6)
-normalPopulation = generateNormalPopulation(0, 1, 10**6)
+mathExpect = 0
+dispersion = 1
 
+population = generatePopulation(0, 1, 10**6)
+normalPopulation = generateNormalPopulation(mathExpect, dispersion, 10**6)
 
-# makeHistogram(population, 10**1, title="Histogram 1")
-# makeHistogram(population, 10**2, title="Histogram 2")
-# makeHistogram(population, 10**3, title="Histogram 3")
-# makeHistogram(population, 10**4, title="Histogram 4")
+uniformDensity = uniform(0, 1).pdf
+normalDensity = norm(mathExpect, np.sqrt(dispersion)).pdf
 
-makeHistogram(normalPopulation, 10**1, title="Histogram 1")
-makeHistogram(normalPopulation, 10**2, title="Histogram 2")
-makeHistogram(normalPopulation, 10**3, title="Histogram 3")
-makeHistogram(normalPopulation, 10**4, title="Histogram 4")
+makeHistogram(population, 10**1, uniformDensity, (-0.2, 1.2), title="Histogram 1")
+makeHistogram(population, 10**2, uniformDensity, (-0.2, 1.2), title="Histogram 2")
+makeHistogram(population, 10**3, uniformDensity, (-0.2, 1.2), title="Histogram 3")
+makeHistogram(population, 10**4, uniformDensity, (-0.2, 1.2), title="Histogram 4")
+
+makeHistogram(normalPopulation, 10**1, normalDensity, (-4, 4), title="Histogram 1")
+makeHistogram(normalPopulation, 10**2, normalDensity, (-4, 4), title="Histogram 2")
+makeHistogram(normalPopulation, 10**3, normalDensity, (-4, 4), title="Histogram 3")
+makeHistogram(normalPopulation, 10**4, normalDensity, (-4, 4), title="Histogram 4")
